f2d639cacee601d7ec5d6674dcc9c52ed1d3a9bf
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-09-11 21:52:42 -0400

    renew

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ Main code
 import numpy as np
 
 
+def finish_sentence(sentence, n, corpus, randomize=False):
+    """
+    inputs:
+        sentence  -> a sentence [list of tokens] that we're trying to build on
+        n         -> [int], the length of n-grams to use for prediction
+        corpus    -> a source corpus [list of tokens]
+        randomize -> a flag indicating whether the process should be randomize [bool]
+
+    return:
+        finished_sentence -> a finished sentence [list of tokens]
+    """
+
+    pass
+
+
 # if __name__ == "__main__":
 # pylint: disable=no-value-for-parameter
 #     add_cli()
