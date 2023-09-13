6e3840921f2c9a839c28582b73255a1081ddb31d
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-09-13 07:58:04 -0400

    test code done

--- test_mtg.py
+++ test_mtg.py
@@ -1,14 +1,48 @@
+"""Markov Text Generator.
+
+Bob Zhang, 2023
+
+Resources:
+Jelinek 1985 "Markov Source Modeling of Text Generation"
 """
-Test goes here
 
-"""
+import nltk
 
-from mtg import finish_sentence as fs
+from mtg import finish_sentence
 
 
-def testCase():
-    pass
+def test_generator():
+    """Test Markov text generator."""
+    corpus = nltk.word_tokenize(nltk.corpus.gutenberg.raw("austen-sense.txt").lower())
+
+    words = finish_sentence(
+        ["she", "was", "not"],
+        3,
+        corpus,
+        randomize=False,
+    )
+    print(words)
+    assert words == [
+        "she",
+        "was",
+        "not",
+        "in",
+        "the",
+        "world",
+        ".",
+    ] or words == [
+        "she",
+        "was",
+        "not",
+        "in",
+        "the",
+        "world",
+        ",",
+        "and",
+        "the",
+        "two",
+    ]
 
 
 if __name__ == "__main__":
-    pass
+    test_generator()
