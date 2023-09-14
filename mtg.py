fbc757e7ac62c05178b06fee95aefb2f7720ea84
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-09-14 02:49:10 -0400

    most done

--- mtg.py
+++ mtg.py
@@ -8,37 +8,31 @@ nltk.download("punkt")
 
 
 def select_next(sentence, n, corpus, random=False):
-    for i in range(len(text) + 1 - n):
+    dict_freq = {}
+    for i in range(len(corpus) + 1 - n):
         ele = corpus[i : i + n]
-        if sen[-1] == ele[: n - 1]:
+        if sentence[-n + 1 :] == ele[: n - 1]:
             if ele[-1] not in dict_freq:
                 dict_freq[ele[-1]] = 1
             else:
                 dict_freq[ele[-1]] += 1
-    dict_freq = sorted(dict_freq.items(), key=lambda x: x[1])
-    return dict_freq
+    if dict_freq:
+        sort_dict = sorted(dict_freq.items(), key=lambda x: x[1], reverse=True)
+        # print(sort_dict)
+        return sort_dict[0][0]
+    elif not dict_freq:
+        pass
 
 
-# at each step choose the sngle most probable next word/token until it yields
-def finish_sentence(sentence, n, corpus, randomize=False):
-    """
-    inputs:
-        sentence  -> a sentence [list of tokens] that we're trying to build on
-        n         -> [int], the length of n-grams to use for prediction
-        corpus    -> a source corpus [list of tokens]
-        randomize -> a flag indicating whether the process should be randomize [bool]
+def finish_sentence(sentence, n, corpus, random=False):
+    curr_sentence = sentence
+    punk = [".", "?", "!"]
+    while len(curr_sentence) < 10 and curr_sentence[-1] not in punk:
+        new_token = select_next(sentence, n, corpus, random=False)
+        curr_sentence.append(new_token)
 
-    return:
-        finished_sentence -> a finished sentence [list of tokens]
-    """
+    return curr_sentence
 
-    pass
-
-
-corpus = nltk.word_tokenize(nltk.corpus.gutenberg.raw("austen-sense.txt").lower())
-sentence = ["I"]
-n = 2
-select_next(sentence, n, corpus, random=False)
 
 # if __name__ == "__main__":
 # pylint: disable=no-value-for-parameter
